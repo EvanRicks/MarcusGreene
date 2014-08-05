0034e6e653661661a0c6e3759b63bb62289211d1
Evan Ricks <evant.ricks@gmail.com>
2014-08-05 16:39:07 -0700

    Added next and previous buttons to overlay and fixed redundancies in script.js

--- js/script.js
+++ js/script.js
@@ -1,46 +1,105 @@
 var main = function() {
 	var frameUp = false;
+	var currentImg;
+	var keycodeRight = 39;
+	var keycodeLeft = 37;
+	var keycodeEsc = 27;
 	$('#gallery img').click(function() {
 		var src = $(this).attr('src');
-		var newSrc = 'url(' + src.substring(0,7) + 'art' + src.substring(13,src.length) + ')';/*changes directory of image from thumbs to art*/
-		console.log(newSrc);
+		var newSrc = thumbToArt(src);
 		$('#frame').css('background-image', newSrc);
-		resizeFrame();
-		$('#frame').fadeIn();
-		$('#overlay').fadeIn();
-		$('footer').fadeOut();
-		frameUp = true;
+		currentImg = $(this).parent();
+		frameUp = allFadeIn();
 	});
-	$('#frame').click(function() { //fade out and shrink frame when frame is clicked
-		shrinkFrame();
-		$(this).fadeOut();
-		$('#overlay').fadeOut();
-		$('footer').fadeIn();
-		frameUp = false;
+	$('#frameContainer').click(function() {
+		frameUp = allFadeOut();
 	});
-	$('#overlay').click(function() { //fade out and shrink frame when overlay is clicked
-		shrinkFrame();
-		$(this).fadeOut();
-		$('#frame').fadeOut();
-		$('footer').fadeIn();
-		frameUp = false;
-	});
-	function resizeFrame() {
-		$('#frameContainer').css({
-			height: $(window).height(),
-			width: $(window).width()
-		});
-		console.log('width: ' + $(window).width());
-	}
-	function shrinkFrame() {
-		$('#frameContainer').css({
-			height: 0,
-			width: 0
-		});
-	}
 	if(frameUp) { //prevents from resizing the frame when it should be shrunk
-		$(window).resize(function() {resizeFrame();});
+		$(window).resize(function() {
+			resizeFrame();
+		});
 	}
+	$('#right').click(function() {
+		currentImg = nextImg(currentImg);
+	});
+	$('#left').click(function() {
+		currentImg = prevImg(currentImg);
+	});
+	$(document).keyup(function(event) {
+		if(frameUp && event.keyCode == keycodeRight) {
+			currentImg = nextImg(currentImg);
+		}
+		else if(frameUp && event.keyCode == keycodeLeft) {
+			currentImg = prevImg(currentImg);
+		}
+		else if(frameUp && event.keyCode == keycodeEsc) {
+			allFadeOut();
+		}
+	});
 }
 
 $(document).ready(main);
+
+function allFadeIn() { //fade in frame, overlay, and arrows, fade out footer, and resize frame
+	resizeFrame();
+	$('#frame').fadeIn();
+	$('#overlay').fadeIn();
+	$('#left').fadeIn();
+	$('#right').fadeIn();
+	$('footer').fadeOut();
+	frameUp = true;
+	return frameUp;
+}
+function allFadeOut() { //fade out frame, overlay, and arrows, fade in footer, and shrink frame
+	shrinkFrame();
+	$('#left').fadeOut();
+	$('#right').fadeOut();
+	$('#overlay').fadeOut();
+	$('#frame').fadeOut();
+	$('footer').fadeIn();
+	frameUp = false;
+	return frameUp;
+}
+function resizeFrame() {
+	$('#frameContainer').css({
+		height: $(window).height(),
+		width: $(window).width()
+	});
+	console.log('width: ' + $(window).width());
+}
+function shrinkFrame() {
+	$('#frameContainer').css({
+		height: 0,
+		width: 0
+	});
+}
+function thumbToArt(src) {
+	var newSrc = 'url(' + src.substring(0,7) + 'art' + src.substring(13,src.length) + ')';
+	return newSrc;
+}
+function nextImg(currentImg) {
+	if (currentImg.is(':last-child')) {
+		var nextImg = $('#gallery li').first();
+	}
+	else {
+		var nextImg = currentImg.next();
+	}
+	var nextSrc = nextImg.children('img').attr('src');
+	nextSrc = thumbToArt(nextSrc);
+	$('#frame').css('background-image', nextSrc);
+	currentImg = nextImg;
+	return currentImg;
+}
+function prevImg(currentImg) {
+	if (currentImg.is(':first-child')) {
+		var prevImg = $('#gallery li').last();
+	}
+	else {
+		var prevImg = currentImg.prev();
+	}
+	var prevSrc = prevImg.children('img').attr('src');
+	prevSrc = thumbToArt(prevSrc);
+	$('#frame').css('background-image', prevSrc);
+	currentImg = prevImg;
+	return currentImg;
+}
