549f313b2d044dd98d7994568ba3408e4a06f0a1
Evan Ricks <evant.ricks@gmail.com>
2014-09-08 14:27:40 -0700

    Fixed broken directories

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@
 	<title>Marcus Greene - CONTACT ME</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="icon" href="image/favicon.ico" type="image/x-icon">
-	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" type="text/css" href="../css/styles.css">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
 </head>
 <body>
--- gallery/figure.php
+++ gallery/figure.php
@@ -1,4 +1,4 @@
-<?php require_once( 'couch/cms.php' ); ?>
+<?php require_once( '../couch/cms.php' ); ?>
 <!DOCTYPE html>
 <html>
 <head>
